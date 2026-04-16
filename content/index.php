71f1433d94e0e1c6ef934e380062961a1703e876
AndestaDewi <andestadewi05@gmail.com>
2026-04-16 19:46:26 +0700

    pertemuan 3 dan 4

---
+++ content/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1 Pemprograman Web</title>
+
+    <style>
+        .container {
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        .heading {
+            border: 1px solid black;
+            display: flex;
+
+        }
+
+        .logo {
+            margin-left: 20px;
+        }
+
+        .judul h1 {
+            font-size: 30px;
+            margin-top: 5px;
+            margin-bottom: 0;
+            margin-left: 20px;
+        }
+
+        .deskripsi {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <!-- Heading -->
+        <div class="heading">
+            <!-- Logo dan Judul -->
+            <div class="logo">
+                <img src="Logounsiq.jpg" alt="logo unsiq" , width="100" , height="100">
+            </div>
+
+            <div class="judul">
+                <h1>Selamat Datang di Mata Kuliah Pemrograman Web</h1>
+                <h1>Pertemuan 2: HTML Dasar</h1>
+            </div>
+        </div>
+
+        <!-- Deskripsi -->
+        <div class="deskripsi">
+            <p>Mata kuliah ini memperkenalkan dasar-dasar pemrograman web menggunakan HTML, CSS, dan JavaScript</p>
+            <p>Pada pertemuan ini, kita fokus pada struktur dan elemen dasar HTML.</p>
+        </div>
+        <hr>
+        <p>HTML adalah bahasa markup yang digunakan untuk membuat struktur halaman web. Dengan HTML, kita bisa menampilkan teks, gambar, dan link secara terorganisir. Untuk Informasi lengkap dari HTML dapat kunungin link berikut: <a href="#">Penjelasan HTML</a></p>
+
+        <!-- Isi -->
+        <h2>Topik Utama Pertemuan Ini:</h2>
+        <ul>
+            <li>Struktur HTML</li>
+            <li>Heading dan Paragraf</li>
+            <li>List (Unordered and Ordered)</li>
+            <li>Link dan Gambar</li>
+            <li>Tabel</li>
+        </ul>
+
+        <h2>Langkah Praktikum:</h2>
+        <ol>
+            <li>Buat file index php</li>
+            <li>Tulis kode HTML</li>
+            <li>Simpan dan buka di browser</li>
+            <ul>
+                <li>buka xampp</li>
+                <li>nyalakan server</li>
+                <li>buka browser dengan url localhost/...</li>
+            </ul>
+            <li>Verifikasi hasil</li>
+        </ol>
+</body>
---
+++ content/pertemuan4.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+
+<body>
+    <h1>Praktikum Pertemuan 4</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Foto</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Andesta Dewi</td>
+            <td>Wonosobo</td>
+            <td>20</td>
+            <td><img src="foto.jpg" alt="Foto Andesta Dewi" width="100"></td>
+        </tr>
+    </table>
+
+    <table border="1" cellpadding="10" cellspacing="0" style="margin-top: 100px;">
+        <thead>
+            <tr>
+                <th colspan="10">Daftar Smartphone</th>
+            </tr>
+            <tr>
+                <th>No</th>
+                <th>Nama Brand</th>
+                <th>Gambar</th>
+                <th>Keterangan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td rowspan="3">1</td>
+                <td rowspan="3">Apple</td>
+                <td rowspan="3"><img src="ip.jpeg" alt="Apple" width="100"></td>
+                <td>iPhone 16 pro max</td>
+            </tr>
+
+            <tr>
+                <td>Dengan desain titanium yang memukau Konytol Kamera, Dollby Vision 4K 120 fps, Dan chip A18 Pro</td>
+            </tr>
+
+            <tr>
+                <td>Rp 20.000.000</td>
+            </tr>
+
+            <tr>
+                <td rowspan="3">2</td>
+                <td rowspan="3">Samsung</td>
+                <td rowspan="3"><img src="samsung.jpeg" alt="Samsung" width="100"></td>
+                <td>Samsung Galaxy S23 Ultra</td>
+            </tr>
+
+            <tr>
+                <td>HP premium dengan layar besar, S Pen, kamera canggih, dan performa sangat mewah</td>
+            </tr>
+
+            <tr>
+                <td>Rp 18.000.000</td>
+            </tr>
+
+            <tr>
+                <td rowspan="3">3</td>
+                <td rowspan="3">Xiaomi</td>
+                <td rowspan="3"><img src="xiaomi.jpeg" alt="Xiaomi" width="100"></td>
+                <td colspan="5">Xiaomi 12 Pro 8/256</td>
+            </tr>
+
+            <tr>
+                <td>HP flagship dengan performa kencang, desain elegan, dan harganya lebih terjangkau</td>
+            </tr>
+
+            <tr>
+                <td>Rp 8.000.000</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
