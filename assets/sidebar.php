83968558d097a2243037095e8f40be516b4f2e10
AndestaDewi <andestadewi05@gmail.com>
2026-05-15 15:18:42 +0700

    Pertemuan 7

---
+++ assets/sidebar.php
@@ -0,0 +1,99 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand">
+        <!--begin::Brand Link-->
+        <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img
+                src="./assets/img/AdminLTELogo.png"
+                alt="AdminLTE Logo"
+                class="brand-image opacity-75 shadow" />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">AdminLTE 4</span>
+            <!--end::Brand Text-->
+        </a>
+        <!--end::Brand Link-->
+    </div>
+    <!--end::Sidebar Brand-->
+    <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+        <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+                class="nav sidebar-menu flex-column"
+                data-lte-toggle="treeview"
+                role="navigation"
+                aria-label="Main navigation"
+                data-accordion="false"
+                id="navigation">
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon bi bi-speedometer"></i>
+                        <p>
+                            Dashboard
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../index.php" class="nav-link active">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="./generate/theme.html" class="nav-link">
+                        <i class="nav-icon bi bi-palette"></i>
+                        <p>Penilaian</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-box-seam-fill"></i>
+                        <p>
+                            Perkuliahan
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../content/pertemuan3.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Pertemuan 3</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../content/pertemuan4.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Pertemuan 4</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../content/pertemuan5.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Pertemuan 5</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../content/pertemuan6.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Pertemuan 6</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../content/pertemuan7.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Pertemuan 7</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!--end::Sidebar Menu-->
+        </nav>
+    </div>
+    <!--end::Sidebar Wrapper-->
+</aside>
